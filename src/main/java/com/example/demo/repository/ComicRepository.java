000463036d4c5e9397393ef952fe182bf819d618
Lam <thaohsk@gmail.com>
2022-12-31 23:21:06 +0700

    fix bug paging

--- src/main/java/com/example/demo/repository/ComicRepository.java
+++ src/main/java/com/example/demo/repository/ComicRepository.java
@@ -18,6 +18,13 @@ public interface ComicRepository extends JpaRepository<ComicEntity, Long> {
             "where (UPPER(c.name) like CONCAT('%',:name,'%') or :name is null)\n" +
             "  and (UPPER(c.author) like CONCAT('%',:author,'%') or :author is null)\n" +
             "  and (UPPER(c.category) like CONCAT('%',:category,'%') or :category is null)\n" +
-            "  and (UPPER(c.publisher) like CONCAT('%',:publisher,'%') or :publisher is null)")
+            "  and (UPPER(c.publisher) like CONCAT('%',:publisher,'%') or :publisher is null)",
+    countQuery = "select count(*)\n" +
+            "from comic c\n" +
+            "where (UPPER(c.name) like CONCAT('%',:name,'%') or :name is null)\n" +
+            "  and (UPPER(c.author) like CONCAT('%',:author,'%') or :author is null)\n" +
+            "  and (UPPER(c.category) like CONCAT('%',:category,'%') or :category is null)\n" +
+            "  and (UPPER(c.publisher) like CONCAT('%',:publisher,'%') or :publisher is null)",
+    )
     Page<ComicEntity> listComic(String name, String category, String author, String publisher, Pageable pageable);
 }
